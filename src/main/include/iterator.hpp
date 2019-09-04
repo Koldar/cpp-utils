e6d490205c81d6629c4e139477d895f6bfb38680
Massimo Bono <massimobono1@gmail.com>
2019-09-04 13:54:40 +0200

    add new iterators and make -> yield a std:pair with references, not pointers

--- src/main/include/iterator.hpp
+++ src/main/include/iterator.hpp
@@ -2,6 +2,7 @@
 #define _ITERATOR_HEADER__
 
 #include <functional>
+#include "varHolder.hpp"
 
 namespace cpp_utils {
 
@@ -75,9 +76,6 @@ class AbstractConstIterator {
  * To surpass this we wrap the iterator inside a ProxyConstIterator, which is the unique owner of the
  * AbstractConstIterator
  * 
- * 
- * @tparam REF 
- * @tparam PTR 
  */
 template <typename REF, typename PTR>
 class ProxyConstIterator {
@@ -117,6 +115,14 @@ public:
     }
 };
 
+//TODO this should be called AbstractContainerBasedConstIterator
+/**
+ * @brief a const iterator that iterate over an underlying container
+ * 
+ * @tparam CONTAINER the type of the underlying container we iterate over
+ * @tparam REF return type (automatically consted) of operator *
+ * @tparam PTR return type (automatically consted) of operator ->  
+ */
 template<typename CONTAINER, typename REF, typename PTR>
 class AbstractNumberContainerBasedConstIterator : public AbstractConstIterator<REF, PTR> {
 public:
@@ -225,6 +231,81 @@ public:
 };
 
 /**
+ * @brief An iterator based on a container which yields not the value in the container, but a function of it.
+ * 
+ * The function used to map teach container value into something else is of type ::MapNumberContainerBasedConstIterator::map_function_t.
+ * 
+ * @tparam CONTAINER the type representing the container
+ * @tparam INTYPE the type which the container is holding
+ * @tparam OUTTYPE the type that this iterator needs to return
+ */
+template <typename CONTAINER, typename INTYPE, typename OUTTYPE>
+class MapNumberContainerBasedConstIterator: public AbstractNumberContainerBasedConstIterator<CONTAINER, OUTTYPE&, OUTTYPE*> {
+    /**
+     * @brief mapping function
+     * 
+     * @param index the index of the element to convert
+     * @param container the container holding the element
+     * @param element the element to be converted
+     * @return the converted value
+     */
+    using map_function_t = std::function<OUTTYPE(int, const CONTAINER&, const INTYPE&)>;
+protected:
+    map_function_t mapFunction;
+    /**
+     * @brief temporary place where to store the output of mapFunction, in order to have a `lvalue`
+     * 
+     */
+    mutable VarHolder<OUTTYPE> tmp;
+    /**
+     * @brief true if the value in `tmp` is the good value for the index `index`. False if we need to call mapFunction to generate it
+     * 
+     */
+    mutable bool isTmpSet;
+public:
+    MapNumberContainerBasedConstIterator(int index, const CONTAINER& container, map_function_t mapFunction) : AbstractNumberContainerBasedConstIterator<CONTAINER, OUTTYPE&, OUTTYPE*>{index, container}, mapFunction{mapFunction}, isTmpSet{false} {
+    }
+    virtual ~MapNumberContainerBasedConstIterator() {
+
+    }
+    MapNumberContainerBasedConstIterator& operator=(const MapNumberContainerBasedConstIterator& other) {
+        this->index = other.index;
+        this->container = other.container;
+        this->mapFunction = other.mapFunction;
+        this->tmp = other.tmp;
+        this->isTmpSet = other.isTmpSet;
+        return *this;
+    }
+    virtual OUTTYPE& operator* () const {
+        if (!isTmpSet) {
+            this->tmp = this->mapFunction(this->index, this->container, this->container[this->index]);
+            this->isTmpSet = true;
+        }
+        
+        return this->tmp;
+    }
+    virtual OUTTYPE* operator->() const {
+        if (!isTmpSet) {
+            this->tmp = this->mapFunction(this->index, this->container, this->container[this->index]);
+            this->isTmpSet = true;
+        }
+        return &this->tmp;
+    }
+public:
+    virtual AbstractNumberContainerBasedConstIterator<CONTAINER, OUTTYPE&, OUTTYPE*>& operator++() {
+        this->isTmpSet = false;
+        return AbstractNumberContainerBasedConstIterator<CONTAINER, OUTTYPE&, OUTTYPE*>::operator++();
+    }
+public:
+    static MapNumberContainerBasedConstIterator<CONTAINER, INTYPE, OUTTYPE> cbegin(const CONTAINER& container, map_function_t mapFunction) {
+        return MapNumberContainerBasedConstIterator<CONTAINER, INTYPE, OUTTYPE>{0, container, mapFunction};
+    }
+    static MapNumberContainerBasedConstIterator<CONTAINER, INTYPE, OUTTYPE> cend(const CONTAINER& container, map_function_t mapFunction) {
+        return MapNumberContainerBasedConstIterator<CONTAINER, INTYPE, OUTTYPE>{-1, container, mapFunction};
+    }
+};
+
+/**
  * @brief An implementation of the AbstractNumberContainerBasedIterator which yields at each iteration pair of data, not the data itself
  * 
  * the first item of the pair is the index in the container while the second item of the pair is the actual value contained
@@ -233,18 +314,18 @@ public:
  * @tparam TYPE type inside the container
  */
 template<typename CONTAINER, typename KEYTYPE, typename VALUETYPE>
-class PairNumberContainerBasedConstIterator: public AbstractNumberContainerBasedConstIterator<CONTAINER, std::pair<KEYTYPE, const VALUETYPE&>, std::pair<KEYTYPE, const VALUETYPE*>*> {
+class PairNumberContainerBasedConstIterator: public AbstractNumberContainerBasedConstIterator<CONTAINER, std::pair<KEYTYPE, const VALUETYPE&>, std::pair<KEYTYPE, const VALUETYPE&>*> {
 private:
-    mutable std::pair<KEYTYPE, const VALUETYPE*> tmp;
+    mutable VarHolder<std::pair<KEYTYPE, const VALUETYPE&>> tmp;
 public:
-    PairNumberContainerBasedConstIterator(int index, const CONTAINER& container) : AbstractNumberContainerBasedConstIterator<CONTAINER, std::pair<KEYTYPE, const VALUETYPE&>, std::pair<KEYTYPE, const VALUETYPE*>*>{index, container} {
+    PairNumberContainerBasedConstIterator(int index, const CONTAINER& container) : AbstractNumberContainerBasedConstIterator<CONTAINER, std::pair<KEYTYPE, const VALUETYPE&>, std::pair<KEYTYPE, const VALUETYPE&>*>{index, container} {
 
     }
     virtual std::pair<KEYTYPE, const VALUETYPE&>  operator* () const {
         return std::pair<KEYTYPE, const VALUETYPE&>{static_cast<KEYTYPE>(this->index), this->container[this->index]};
     }
-    virtual std::pair<KEYTYPE, const VALUETYPE*>* operator->() const {
-        this->tmp = std::pair<KEYTYPE, const VALUETYPE*>{static_cast<KEYTYPE>(this->index), &this->container[this->index]};
+    virtual std::pair<KEYTYPE, const VALUETYPE&>* operator->() const {
+        this->tmp = std::pair<KEYTYPE, const VALUETYPE&>{static_cast<KEYTYPE>(this->index), this->container[this->index]};
         return &tmp;
     }
 public:
