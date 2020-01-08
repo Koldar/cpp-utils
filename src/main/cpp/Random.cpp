287af8c82378b26d2cc232e0f8c695d50a12b728
Massimo Bono <massimobono1@gmail.com>
2020-01-08 17:34:01 +0100

    random now uses C++ random utilitiy internally

--- src/main/cpp/Random.cpp
+++ src/main/cpp/Random.cpp
@@ -1,43 +1,78 @@
 
 #include "Random.hpp"
 #include <time.h>
+#include <limits>
 
 
 namespace cpp_utils {
 
-Random::Random() {
-}
-
-Random::Random(unsigned int seed) {
-    srand(seed);
-}
-
-Random::Random(const Random& o) {
-
-}
-
-Random::Random(Random&& o) {
-
-}
-
-Random& Random::operator = (const Random& o) {
-    return *this;
-}
-
-Random& Random::operator = (Random&& o) {
-    return *this;
-}
-
-Random::~Random() {
-
-}
-
-bool Random::flip(double successProbability) {
-    if (cpp_utils::isApproximatelyEqual(successProbability, 1.0, 1e-6)) {
-        return true;
+    Random::Random() {
     }
-    auto rnd = rand();
-    return rnd < (successProbability * RAND_MAX);
-}
+
+    Random::Random(unsigned int seed) : uniformIntDistributions{}, uniformRealDistributions{}, randomEngine{seed} {
+        srand(seed);
+    }
+
+    Random::Random(const Random& o) : uniformIntDistributions{o.uniformIntDistributions}, uniformRealDistributions{o.uniformRealDistributions}, randomEngine{o.randomEngine} {
+
+    }
+
+    Random::Random(Random&& o) : uniformIntDistributions{::std::move(o.uniformIntDistributions)}, uniformRealDistributions{::std::move(o.uniformRealDistributions)}, randomEngine{::std::move(o.randomEngine)}{
+
+    }
+
+    Random& Random::operator = (const Random& o) {
+        this->uniformRealDistributions = o.uniformRealDistributions;
+        this->uniformIntDistributions = o.uniformIntDistributions;
+        this->randomEngine = o.randomEngine;
+        return *this;
+    }
+
+    Random& Random::operator = (Random&& o) {
+        this->uniformRealDistributions = ::std::move(o.uniformRealDistributions);
+        this->uniformIntDistributions = ::std::move(o.uniformIntDistributions);
+        this->randomEngine = ::std::move(o.randomEngine);
+        return *this;
+    }
+
+    Random::~Random() {
+
+    }
+
+    bool Random::flip(double successProbability) {
+        if (cpp_utils::isApproximatelyEqual(successProbability, 1.0, 1e-6)) {
+            return true;
+        }
+        auto rnd = rand();
+        return rnd < (successProbability * RAND_MAX);
+    }
+
+    double Random::nextDouble(double lowerbound, double upperbound, bool includeUpperbound) {
+        auto key = std::make_tuple(lowerbound, upperbound, includeUpperbound);
+        if (!this->uniformRealDistributions.containsKey(key)) {
+            if (includeUpperbound) {
+                //uniform_reals_distribution picks from [a,b)
+                this->uniformRealDistributions.put(key, std::uniform_real_distribution<double>{lowerbound, upperbound});
+            } else {
+                this->uniformRealDistributions.put(key, std::uniform_real_distribution<double>{lowerbound, std::nextafter(upperbound, std::numeric_limits<double>::max())});
+            }
+        }
+        return this->uniformRealDistributions[key](this->randomEngine);
+    }
+    
+    int Random::nextInt(int lowerbound, int upperbound, bool includeUpperbound) {
+        //uniform_int_distribution picks from [a,b]
+        upperbound -= includeUpperbound ? 0 : 1;
+        auto key = std::make_tuple(lowerbound, upperbound);
+        if (!this->uniformIntDistributions.containsKey(key)) {
+            this->uniformIntDistributions.put(key, std::uniform_int_distribution<int>{lowerbound, upperbound});
+        }
+        return this->uniformIntDistributions[key](this->randomEngine);
+    }
+
+
+    int Random::nextInt(const Interval<int>& interval) {
+        return this->nextInt(interval.getLB(), interval.getUB(), true);
+    }
 
 }
--- src/main/include/Random.hpp
+++ src/main/include/Random.hpp
@@ -1,9 +1,13 @@
 #ifndef _CPP_UTILS_RANDOM_HEADER__
 #define _CPP_UTILS_RANDOM_HEADER__
 
+#include <random>
+
 #include "exceptions.hpp"
 #include "Interval.hpp"
 #include "math.hpp"
+#include "mapplus.hpp"
+#include "operators.hpp"
 
 namespace cpp_utils {
 
@@ -22,47 +26,63 @@ namespace cpp_utils {
         Random(unsigned int seed);
         Random(const Random& o);
         Random(Random&& o);
+    public:
         Random& operator = (const Random& o);
         Random& operator = (Random&& o);
         virtual ~Random();
     public:
         template <typename T>
         static T nextNum(T lowerbound, T upperbound, bool includeUpperbound = false) {
-            return Random{}.next(lowerbound, upperbound, includeUpperbound);
+            return Random{}.nextInt(lowerbound, upperbound, includeUpperbound);
         }
         template <typename T>
         static T nextNum(const Interval<T>& interval) {
-            return Random{}.next(interval);
+            return Random{}.nextInt(interval);
         }
     public:
-        template <typename T>
-        T next(T lowerbound, T upperbound, bool includeUpperbound = false) {
-            if (includeUpperbound) {
-                upperbound += 1;
-            }
-            T diff = upperbound - lowerbound;
-            if (diff <= 0) {
-                debug("lowerbound =", lowerbound);
-                debug("upperbound =", upperbound);
-                throw cpp_utils::exceptions::ImpossibleException{"upperbound less than lowerbund!"};
-            }
-            T x = (RAND_MAX + 0u) / diff; //number of times u-l can be put inside RAND_MAX
-            T y = x * diff; //obtain a valule smaller than rand_max
-            T r;
-            do {
-                r = rand();
-                debug("r is", r, "y=", y);
-            } while(r >= y); //if rand is greater than the admissible value, retry
-            return lowerbound + (r / x);
+        double nextDouble(double lowerbound, double upperbound, bool includeUpperbound = false);
+        int nextInt(int lowerbound, int upperbound, bool includeUpperbound = false);
+        // template <typename T>
+        // T next(T lowerbound, T upperbound, bool includeUpperbound = false) {
+        //     if (includeUpperbound) {
+        //         upperbound += 1;
+        //     }
+        //     T diff = upperbound - lowerbound;
+        //     if (diff <= 0) {
+        //         debug("lowerbound =", lowerbound);
+        //         debug("upperbound =", upperbound);
+        //         throw cpp_utils::exceptions::ImpossibleException{"upperbound less than lowerbund!"};
+        //     }
+        //     T x = (RAND_MAX + 0u) / diff; //number of times u-l can be put inside RAND_MAX
+        //     T y = x * diff; //obtain a valule smaller than rand_max
+        //     T r;
+        //     do {
+        //         r = rand();
+        //         debug("r is", r, "y=", y);
+        //     } while(r >= y); //if rand is greater than the admissible value, retry
+        //     return lowerbound + (r / x);
 
-        }
-        template <typename T>
-        T next(const Interval<T>& interval) {
-            return this->next(interval.getLB(), interval.getUB(), true);
-        }
+        // }
+        /**
+         * @brief fetch a value in a interval [a, b]
+         * 
+         * @param interval the involved interval
+         * @return int a value in the interval
+         */
+        int nextInt(const Interval<int>& interval);
+        /**
+         * @brief flip a coin which has the given probability to return OK and 1- the given probability to return KO
+         * 
+         * @param successProbability probability to yield true
+         * @return true 
+         * @return false 
+         */
         bool flip(double successProbability);
+    private:
+        MapPlus<std::tuple<double, double, bool>, std::uniform_real_distribution<double>> uniformRealDistributions;
+        MapPlus<std::tuple<int, int>, std::uniform_int_distribution<int>> uniformIntDistributions;
+        std::mt19937_64 randomEngine;
     };
-
 }
 
 #endif
