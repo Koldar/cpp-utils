476bc0fccc11258c9c6e61c83ae97a0ef0237a3d
Massimo Bono <massimobono1@gmail.com>
2020-01-14 17:45:14 +0100

    add more lambda tests

--- src/test/cpp/testLambda.cpp
+++ src/test/cpp/testLambda.cpp
@@ -14,8 +14,21 @@ int use(const FROM& f, LAMBDA mapper) {
     return mapper(f);
 }
 
+
+template <typename E>
+class Bar {
+public:
+    virtual int use(E e, std::function<int(const E&)> mapper) {
+        return mapper(e);
+    }
+};
+
+
 SCENARIO("test lambdas") {
 
     REQUIRE(use(MyFoo1{}, [&](const MyFoo1&) { return 1; }) == 1);
     REQUIRE(use(MyFoo2{}, [&](const MyFoo2&) { return 2; }) == 2);
+
+    auto b = Bar<char>{};
+    REQUIRE(b.use('a', [&](const char& e) {return 3;}));
 }
