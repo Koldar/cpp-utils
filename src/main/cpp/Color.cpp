980badc89b2f8817bfd6db085733efdc82f14692
Massimo Bono <m.bono@unibs.it>
2020-01-17 18:49:50 +0100

    add new color api

--- src/main/cpp/Color.cpp
+++ src/main/cpp/Color.cpp
@@ -2,14 +2,60 @@
 
 namespace cpp_utils {
 
+    const color_t color_t::LIGHT_RED = RED.lighter(0.2);
+    const color_t color_t::RED = color_t{wrapped_uchar{250}, wrapped_uchar{0}, wrapped_uchar{0}};
+    const color_t color_t::DARK_RED = RED.darker(0.2);
+
+    const color_t color_t::LIGHT_GREEN = GREEN.lighter(0.2);
+    const color_t color_t::GREEN = color_t{wrapped_uchar{0}, wrapped_uchar{255}, wrapped_uchar{0}};
+    const color_t color_t::DARK_GREEN = GREEN.darker(0.2);
+
+    const color_t color_t::LIGHT_BLUE = BLUE.lighter(0.2);
+    const color_t color_t::BLUE = color_t{wrapped_uchar{0}, wrapped_uchar{0}, wrapped_uchar{255}};
+    const color_t color_t::DARK_BLUE = BLUE.darker(0.2);
+
+    const color_t color_t::LIGHT_CYAN = CYAN.lighter(0.2);
+    const color_t color_t::CYAN = color_t{wrapped_uchar{0}, wrapped_uchar{255}, wrapped_uchar{255}};
+    const color_t color_t::DARK_CYAN = CYAN.darker(0.2);
+
+    const color_t color_t::LIGHT_VIOLET = VIOLET.lighter(0.2);
+    const color_t color_t::VIOLET = color_t{wrapped_uchar{255}, wrapped_uchar{0}, wrapped_uchar{255}};
+    const color_t color_t::DARK_VIOLET = VIOLET.darker(0.2);
+
+    const color_t color_t::LIGHT_SKY = SKY.lighter(0.2);
+    const color_t color_t::SKY = color_t{wrapped_uchar{0}, wrapped_uchar{128}, wrapped_uchar{255}};
+    const color_t color_t::DARK_SKY = SKY.darker(0.2);
+
+    const color_t color_t::LIGHT_GRASS = GRASS.lighter(0.2);
+    const color_t color_t::GRASS = color_t{wrapped_uchar{0}, wrapped_uchar{255}, wrapped_uchar{128}};
+    const color_t color_t::DARK_GRASS = GRASS.darker(0.2);
+    
+    const color_t color_t::LIGHT_PURPLE = PURPLE.lighter(0.2);
+    const color_t color_t::PURPLE = color_t{wrapped_uchar{128}, wrapped_uchar{0}, wrapped_uchar{255}};
+    const color_t color_t::DARK_PURPLE = PURPLE.darker(0.2);
+
+    const color_t color_t::LIGHT_PINKISH = PINKISH.lighter(0.2);
+    const color_t color_t::PINKISH = color_t{wrapped_uchar{255}, wrapped_uchar{0}, wrapped_uchar{128}};
+    const color_t color_t::DARK_PINKISH = PINKISH.darker(0.2);
+    
+    const color_t color_t::LIGHT_YELLOW = YELLOW.lighter(0.2);
+    const color_t color_t::YELLOW = color_t{wrapped_uchar{255}, wrapped_uchar{255}, wrapped_uchar{0}};
+    const color_t color_t::DARK_YELLOW = YELLOW.darker(0.2);
+    
+    const color_t color_t::LIGHT_ORANGE = ORANGE.lighter(0.2);
+    const color_t color_t::ORANGE = color_t{wrapped_uchar{255}, wrapped_uchar{128}, wrapped_uchar{0}};
+    const color_t color_t::DARK_ORANGE = ORANGE.darker(0.2);
+    
+    const color_t color_t::LIGHT_GREY = GREY.lighter(0.2);
+    const color_t color_t::GREY = color_t{wrapped_uchar{128}, wrapped_uchar{128}, wrapped_uchar{128}};
+    const color_t color_t::DARK_GREY = GREY.darker(0.2);
+
     const color_t color_t::BLACK = color_t{wrapped_uchar{0}, wrapped_uchar{0}, wrapped_uchar{0}};
     const color_t color_t::WHITE = color_t{wrapped_uchar{255}, wrapped_uchar{255}, wrapped_uchar{255}};
 
-    const color_t color_t::RED = color_t{wrapped_uchar{255}, wrapped_uchar{0}, wrapped_uchar{0}};
-    const color_t color_t::GREEN = color_t{wrapped_uchar{0}, wrapped_uchar{255}, wrapped_uchar{0}};
-    const color_t color_t::BLUE = color_t{wrapped_uchar{0}, wrapped_uchar{0}, wrapped_uchar{255}};
-
-    const color_t color_t::CYAN = color_t{wrapped_uchar{0}, wrapped_uchar{255}, wrapped_uchar{255}};
+    const color_t color_t::LIGHT_BROWN = BROWN.lighter(0.2);
+    const color_t color_t::BROWN = color_t{wrapped_uchar{102}, wrapped_uchar{51}, wrapped_uchar{0}};
+    const color_t color_t::DARK_BROWN = BROWN.darker(0.2);
 
     color_t::color_t() : red{0}, green{0}, blue{0} {
 
@@ -46,47 +92,136 @@ namespace cpp_utils {
         return this->blue;
     }
 
+    color_t color_t::lighter(double scale) const {
+        color_t result{*this};
+        return result.lighter(scale);
+    }
+
+    color_t& color_t::lighter(double scale) {
+        color_t diff = color_t{wrapped_uchar{255} - this->red, wrapped_uchar{255} - this->green, wrapped_uchar{255} - this->blue};
+        diff = diff.scale(scale);
+        diff = color_t{diff.red + this->red, diff.green + this->green, diff.blue + this->blue};
+        this->red = diff.red;
+        this->green = diff.green;
+        this->blue = diff.blue;
+        return *this;
+    }
+
+    color_t color_t::darker(double scale) const {
+        color_t result{*this};
+        return result.darker(scale);
+    }
+
+    color_t& color_t::darker(double scale) {
+        return this->scale(1. - scale);
+    }
+
+    color_t color_t::scale(double scale) const {
+        color_t result{*this};
+        return result.scale(scale);
+    }
+	
+    color_t& color_t::scale(double scale) {
+        this->red *= scale;
+        this->green *= scale;
+        this->blue *= scale;
+        return *this;
+    }
+
+    color_t color_t::invert() const {
+        color_t result{*this};
+
+        return result.invert();
+    }
+
+    color_t& color_t::invert() {
+        this->red = wrapped_uchar{255} - this->red;
+        this->green = wrapped_uchar{255} - this->green;
+        this->blue = wrapped_uchar{255} - this->blue;
+
+        return *this;
+    }
+
+    color_t color_t::min(const color_t& other) const {
+        color_t result{*this};
+        return result.min(other);
+    }
+
+    color_t& color_t::min(const color_t& other) {
+        this->red = std::min(this->red, other.red);
+        this->green = std::min(this->green, other.green);
+        this->blue = std::min(this->blue, other.blue);
+        return *this;
+    }
+
+    color_t color_t::max(const color_t& other) const {
+        color_t result{*this};
+        return result.max(other);
+    }
+
+    color_t& color_t::max(const color_t& other) {
+        this->red = std::max(this->red, other.red);
+        this->green = std::max(this->green, other.green);
+        this->blue = std::max(this->blue, other.blue);
+        return *this;
+    }
+
+    color_t color_t::lerp(const color_t& other) const {
+        color_t result{*this};
+        return result.lerp(other);
+    }
+
+	color_t& color_t::lerp(const color_t& other) {
+        this->red = (this->red + other.red)/2;
+        this->green = (this->green + other.green)/2;
+        this->blue = (this->blue + other.blue)/2;
+
+        return *this;
+    }
+
     color_t color_t::merge(const color_t& other) const {
         color_t result{*this};
         return result.merge(other);
     }
 
     color_t& color_t::merge(const color_t& other) {
-        wrapped_uchar minRed;
-        wrapped_uchar maxRed;
-        wrapped_uchar minGreen;
-        wrapped_uchar maxGreen;
-        wrapped_uchar minBlue;
-        wrapped_uchar maxBlue;
-        if (this->red > other.red) {
-            minRed = other.red;
-            maxRed = this->red;
-        } else {
-            minRed = this->red;
-            maxRed = other.red;
-        }
+        return this->lerp(other);
+        //TODO remove
+        // wrapped_uchar minRed;
+        // wrapped_uchar maxRed;
+        // wrapped_uchar minGreen;
+        // wrapped_uchar maxGreen;
+        // wrapped_uchar minBlue;
+        // wrapped_uchar maxBlue;
+        // if (this->red > other.red) {
+        //     minRed = other.red;
+        //     maxRed = this->red;
+        // } else {
+        //     minRed = this->red;
+        //     maxRed = other.red;
+        // }
 
-        if (this->green > other.green) {
-            minGreen = other.green;
-            maxGreen = this->green;
-        } else {
-            minGreen = this->green;
-            maxGreen = other.green;
-        }
+        // if (this->green > other.green) {
+        //     minGreen = other.green;
+        //     maxGreen = this->green;
+        // } else {
+        //     minGreen = this->green;
+        //     maxGreen = other.green;
+        // }
 
-        if (this->blue > other.blue) {
-            minBlue = other.blue;
-            maxBlue = this->blue;
-        } else {
-            minBlue = this->blue;
-            maxBlue = other.blue;
-        }
+        // if (this->blue > other.blue) {
+        //     minBlue = other.blue;
+        //     maxBlue = this->blue;
+        // } else {
+        //     minBlue = this->blue;
+        //     maxBlue = other.blue;
+        // }
 
-        this->red = minRed + (maxRed - minRed)/2;
-        this->green = minGreen + (maxGreen - minGreen)/2;
-        this->blue = minBlue + (maxBlue - minBlue)/2;
+        // this->red = minRed + (maxRed - minRed)/2;
+        // this->green = minGreen + (maxGreen - minGreen)/2;
+        // this->blue = minBlue + (maxBlue - minBlue)/2;
 
-        return *this;
+        // return *this;
     }
 
     std::ostream& operator << (std::ostream& out, const color_t& c) {
--- src/main/include/Color.hpp
+++ src/main/include/Color.hpp
@@ -11,12 +11,60 @@ namespace cpp_utils {
 	public:
 		friend std::ostream& operator << (std::ostream& out, const color_t& c);
 	public:
+		static const color_t LIGHT_RED;
+		static const color_t RED;
+		static const color_t DARK_RED;
+
+		static const color_t LIGHT_GREEN;
+		static const color_t GREEN;
+		static const color_t DARK_GREEN;
+
+		static const color_t LIGHT_BLUE;
+		static const color_t BLUE;
+		static const color_t DARK_BLUE;
+
+		static const color_t LIGHT_ORANGE;
+		static const color_t ORANGE;
+		static const color_t DARK_ORANGE;
+
+		static const color_t LIGHT_YELLOW;
+		static const color_t YELLOW;
+		static const color_t DARK_YELLOW;
+
+		static const color_t LIGHT_CYAN;
+		static const color_t CYAN;
+		static const color_t DARK_CYAN;
+
+		static const color_t LIGHT_PURPLE;
+		static const color_t PURPLE;
+		static const color_t DARK_PURPLE;
+
+		static const color_t LIGHT_VIOLET;
+		static const color_t VIOLET;
+		static const color_t DARK_VIOLET;
+
+		static const color_t LIGHT_GRASS;
+		static const color_t GRASS;
+		static const color_t DARK_GRASS;
+
+		static const color_t LIGHT_SKY;
+		static const color_t SKY;
+		static const color_t DARK_SKY;
+
+		static const color_t LIGHT_PINKISH;
+		static const color_t PINKISH;
+		static const color_t DARK_PINKISH;
+
+		static const color_t LIGHT_BROWN;
+		static const color_t BROWN;
+		static const color_t DARK_BROWN;
+		
+		static const color_t LIGHT_GREY;
+		static const color_t GREY;
+		static const color_t DARK_GREY;
+
 		static const color_t BLACK;
 		static const color_t WHITE;
-		static const color_t RED;
-		static const color_t GREEN;
-		static const color_t BLUE;
-		static const color_t CYAN;
 	public:
 		color_t();
 		color_t(const wrapped_uchar& r, const wrapped_uchar& g, const wrapped_uchar& b);
@@ -28,6 +76,142 @@ namespace cpp_utils {
 		wrapped_uchar getGreen() const;
 		wrapped_uchar getBlue() const;
 	public:
+
+		/**
+		 * @brief make a color lighter
+		 * 
+		 * @param scale a value between 0 and 1. for instance 0.2 means make the color lighter by 20%
+		 * @return color_t& new color
+		 */
+		color_t lighter(double scale) const;
+
+		/**
+		 * @brief make a color lighter
+		 * 
+		 * @param scale a value between 0 and 1. for instance 0.2 means make the color lighter by 20%
+		 * @return color_t& this
+		 */
+		color_t& lighter(double scale);
+
+		/**
+		 * @brief make a color darker
+		 * 
+		 * @param scale a value between 0 and 1. for instance 0.2 means make the color darker by 20%
+		 * @return color_t& new color
+		 */
+		color_t darker(double scale) const;
+
+		/**
+		 * @brief make a color darker
+		 * 
+		 * @param scale a value between 0 and 1. for instance 0.2 means make the color darker by 20%
+		 * @return color_t& this
+		 */
+		color_t& darker(double scale);
+
+		/**
+		 * @brief multiply each component with the given multiplier
+		 * 
+		 * @code
+		 * 	color_t{100,10,20}.scale(1.2) //120, 12, 25
+		 * @endcode
+		 * 
+		 * @param scale multipler. if the multiplication exceeds 255, it is truncated to 255
+		 * @return color_t a new color
+		 */
+		color_t scale(double scale) const;
+		/**
+		 * @brief multiply each component with the given multiplier
+		 * 
+		 * @code
+		 * 	color_t{100,10,20}.scale(1.2) //120, 12, 25
+		 * @endcode
+		 * 
+		 * @param scale multipler. if the multiplication exceeds 255, it is truncated to 255
+		 * @return color_t this
+		 */
+		color_t& scale(double scale);
+		/**
+		 * @brief invert each component fo the color
+		 * 
+		 * @code
+		 *  {255,123,6}.invert() //0,132,249
+		 * @endcode
+		 * 
+		 * @return color_t a new color
+		 */
+		color_t invert() const;
+
+		/**
+		 * @brief invert each component fo the color
+		 * 
+		 * @code
+		 *  {255,123,6}.invert() //0,132,249
+		 * @endcode
+		 * 
+		 * @return color_t this
+		 */
+		color_t& invert();
+
+		/**
+		 * @brief compute a new color based on the minimum of each components of the colors
+		 * 
+		 * @code
+		 *  {255,123,6}.min(6, 123, 127) //6,123,6
+		 * @endcode
+		 * 
+		 * @param other the other colors involved
+		 * @return color_t new color
+		 */
+		color_t min(const color_t& other) const;
+
+		/**
+		 * @brief compute a new color based on the minimum of each components of the colors
+		 * 
+		 * @code
+		 *  {255,123,6}.min(6, 123, 127) //6,123,6
+		 * @endcode
+		 * 
+		 * @param other the other colors involved
+		 * @return color_t this
+		 */
+		color_t& min(const color_t& other);
+
+		/**
+		 * @brief compute a new color based on the maximum of each components of the colors
+		 * 
+		 * @code
+		 *  {255,123,6}.max(6, 123, 127) //255,123,127
+		 * @endcode
+		 * 
+		 * @param other the other colors involved
+		 * @return color_t new color
+		 */
+		color_t max(const color_t& other) const;
+
+		/**
+		 * @brief compute a new color based on the maximum of each components of the colors
+		 * 
+		 * @code
+		 *  {255,123,6}.max(6, 123, 127) //255,123,127
+		 * @endcode
+		 * 
+		 * @param other the other colors involved
+		 * @return color_t this
+		 */
+		color_t& max(const color_t& other);
+
+		color_t lerp(const color_t& other) const;
+		color_t& lerp(const color_t& other);
+
+		/**
+		 * @brief perform a lerp between 2 colors
+		 * 
+		 * 
+		 * 
+		 * @param other 
+		 * @return color_t 
+		 */
 		color_t merge(const color_t& other) const;
 		color_t& merge(const color_t& other);
 	private:
