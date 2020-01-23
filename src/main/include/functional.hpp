297f18f3fb8579ed52d23967a9dc336703167413
Massimo Bono <massimobono1@gmail.com>
2020-01-23 11:11:42 +0100

    add new java functors

--- src/main/include/functional.hpp
+++ src/main/include/functional.hpp
@@ -32,6 +32,20 @@ namespace cpp_utils {
     template <typename IN, typename OUT>
     using function_t = std::function<OUT(const IN& in)>;
 
+    /**
+     * @brief a function whic accept a certain type and return the same type
+     * 
+     * Doesn't encessary mean that it is the identify function. For isntance,
+     * 
+     * @code
+     *  autofunction_t<int> double = [&](const int& a) { return 2*a; };
+     * @endcode
+     * 
+     * @tparam X 
+     */
+    template <typename X>
+    using autofunction_t =  function_t<X, X>;
+
     template <typename IN>
     using predicate_t = std::function<bool(const IN& in)>;
 
@@ -39,6 +53,12 @@ namespace cpp_utils {
 
     template <typename IN>
     using consumer_t = std::function<void(const IN& in)>;
+
+    template <typename IN1, typename IN2>
+    using biconsumer_t = std::function<void(const IN1& in, const IN2& in2)>;
+
+    template <typename IN1, typename IN2, typename IN3>
+    using triconsumer_t = std::function<void(const IN1& in, const IN2& in2, const IN3& in3)>;
 }
 
 #endif 
