706f428e4b526a7199aa29cfeaaec4c7e41dd0b6
Massimo Bono <massimobono1@gmail.com>
2020-02-04 14:13:53 +0100

    updalote test logging

--- src/test/cpp/testLog.cpp
+++ src/test/cpp/testLog.cpp
@@ -10,6 +10,12 @@ SCENARIO("test log.h") {
     //normal
     critical("this is a test!", 20);
 
+    int a = 56;
+    float b = 3.4;
+    char c = 'd';
 
+    auto_critical(a);
+    auto_critical(b);
+    auto_critical(c);
     
 }
